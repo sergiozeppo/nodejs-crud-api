9440837a5724c2c0cace31ba4a1eefa491c2ea86
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-05-11 22:36:44 +0500

    feat: add initial userDB

---
+++ src/utils/userDB.ts
@@ -0,0 +1,34 @@
+import { User } from '../types/types'
+
+export const users: User[] = [
+  {
+    id: 'a1b2c3d4',
+    username: 'Alice123',
+    age: 25,
+    hobbies: ['reading', 'hiking', 'coding'],
+  },
+  {
+    id: 'e5f6g7h8',
+    username: 'BobTheBuilder',
+    age: 30,
+    hobbies: ['building', 'gaming'],
+  },
+  {
+    id: 'i9j0k1l2',
+    username: 'CharlieBrown',
+    age: 20,
+    hobbies: ['music', 'drawing', 'sports'],
+  },
+  {
+    id: 'm3n4o5p6',
+    username: 'DavidTheGamer',
+    age: 28,
+    hobbies: ['gaming', 'coding', 'movies'],
+  },
+  {
+    id: 'q7r8s9t0',
+    username: 'EveTheReader',
+    age: 32,
+    hobbies: ['reading', 'writing', 'traveling'],
+  },
+]
