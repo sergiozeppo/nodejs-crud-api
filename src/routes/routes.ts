f7eec88b529cd50623e5d8b35fd9617187b12dc0
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2025-05-13 00:10:15 +0500

    fix: add 500 status code

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -13,32 +13,37 @@ export function handleRoutes(
   request: IncomingMessage,
   response: ServerResponse,
 ): void {
-  const { pathname = '', query } = parse(request.url || '', true)
-  const method = request.method || 'GET'
+  try {
+    const { pathname = '' } = parse(request.url || '', true)
+    const method = request.method || 'GET'
 
-  if (pathname === endpoint) {
-    switch (method) {
-      case 'GET':
-        return getUsersController(response)
-      case 'POST':
-        return addUserController(request, response)
+    if (pathname === endpoint) {
+      switch (method) {
+        case 'GET':
+          return getUsersController(response)
+        case 'POST':
+          return addUserController(request, response)
+      }
     }
-  }
 
-  const match = pathname?.match(endpointMatch)
-  const id = match?.[1]
+    const match = pathname?.match(endpointMatch)
+    const id = match?.[1]
 
-  if (id) {
-    switch (method) {
-      case 'GET':
-        return getUserIDController(response, id)
-      case 'PUT':
-        return updateUserController(request, response, id)
-      case 'DELETE':
-        return deleteUserController(response, id)
+    if (id) {
+      switch (method) {
+        case 'GET':
+          return getUserIDController(response, id)
+        case 'PUT':
+          return updateUserController(request, response, id)
+        case 'DELETE':
+          return deleteUserController(response, id)
+      }
     }
-  }
 
-  response.writeHead(404, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify({ message: 'Endpoint not found' }))
+    response.writeHead(404, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify({ message: 'Endpoint not found' }))
+  } catch {
+    response.writeHead(500, { 'Content-Type': 'application/json' })
+    response.end(JSON.stringify({ message: 'Internal server error' }))
+  }
 }
